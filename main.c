13b6e6bf956c1fdc1faa76b5a953d05022df8556
Beqa Tvildiani <btvildia@c2r13s3.42wolfsburg.de>
2024-05-28 22:57:23 +0200

    ft_class

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: btvildia <btvildia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/27 21:53:28 by btvildia          #+#    #+#             */
-/*   Updated: 2024/05/28 22:55:35 by btvildia         ###   ########.fr       */
+/*   Updated: 2024/05/28 22:56:57 by btvildia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,8 @@ int	main(void)
 	cat = class();
 	add(cat, 1, "the_name", "Cat");
 	add(cat, 3, "the_age", &(int){5});
-	printf("%s\n", *(char **)cat->attr(cat, "the_name"));
-	printf("%d\n", *(int *)cat->attr(cat, "the_age"));
+	printf("Name: %s\n", get_string(cat, "the_name"));
+	printf("Age: %d\n", get_int(cat, "the_age"));
 	destroy_class(cat);
 	return (0);
 }
